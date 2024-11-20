7bfb4307d684c96e6b932b80cf4dfff0ef115d34
Juf19 <jorgeufer.19@gmail.com>
2024-11-19 22:59:45 -0500

    problema App resuleto

--- Mockup/React/nanec/src/index.js
+++ Mockup/React/nanec/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 //import App1 from './App1';
 //import App2 from './App2';
-import App3 from './App3';
+//import App3 from './App3';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    
-    <App3 />
+    <App />
   </React.StrictMode>
 );
 
