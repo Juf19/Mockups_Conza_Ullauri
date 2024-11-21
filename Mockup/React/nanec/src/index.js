be966bb852a4b6d37b912bf5fc000f742a6489d2
Juf19 <jorgeufer.19@gmail.com>
2024-11-20 22:29:02 -0500

    Mockups avance3

--- Mockup/React/nanec/src/index.js
+++ Mockup/React/nanec/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-//import App1 from './App1';
+//import App from './App';
+import App1 from './App1';
 //import App2 from './App2';
 //import App3 from './App3';
+//import App4 from './App4';
+//import App5 from './App5';
+//import App6 from './App6';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    
-    <App />
+    <App1 />
   </React.StrictMode>
 );
 
